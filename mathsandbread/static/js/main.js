bbe64091d1269efe2af4cd0868029a7524963ad4
admin <mailtosungkwon@gmail.com>
2019-07-28 15:41:11 +0100

    Add clickevent on js-stack

--- mathsandbread/static/js/main.js
+++ mathsandbread/static/js/main.js
@@ -1,1 +1,26 @@
 console.log('main.js')
+
+const toggleContentClass = content => {
+  return [...content.classList].includes('show')?
+  content.classList.remove('show'):
+  content.classList.add('show');
+}
+
+const onStackClick = e => {
+  const classList = [...e.target.classList]
+  let content;
+  if (classList.includes('row')){
+    content = e.target.nextElementSibling
+  } else if(classList.includes('column')){
+    content = e.target.parentElement.nextElementSibling
+  }
+  toggleContentClass(content)
+  return;
+}
+
+const jsStacks = document.querySelectorAll('.js-stack');
+
+jsStacks.forEach(stack => {
+  stack.addEventListener('click', onStackClick)
+})
+
